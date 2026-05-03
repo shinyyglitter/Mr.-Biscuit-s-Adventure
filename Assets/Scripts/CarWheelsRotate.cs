cafe4919e96fc8228dd836430f2075567aba537e
hogstol03 <juliabhogstol@hotmail.com>
2026-05-03 19:50:25 +0200

    Added rotation to the car wheels

---
+++ Assets/Scripts/CarWheelsRotate.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CarWheelsRotate : MonoBehaviour
+{
+    
+    private float rotateSpeed = 300f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
+    }
+}
