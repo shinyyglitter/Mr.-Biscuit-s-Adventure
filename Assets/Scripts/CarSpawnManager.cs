79d8df505e1a648511d2d7e9bbbc4233769bb164
hogstol03 <juliabhogstol@hotmail.com>
2026-05-09 21:59:40 +0200

    Added repeated spawning of cars + intervals
    Cars now spawn with a random interval of 2 to 5. Also did some tidying of the code in the Car SpawnManager script.

--- Assets/Scripts/CarSpawnManager.cs
+++ Assets/Scripts/CarSpawnManager.cs
@@ -4,28 +4,27 @@ public class CarSpawnManager : MonoBehaviour
 {
     public GameObject[] carPrefabs;
     public Transform carSpawnPoint;
-    //private float spawnRangeX = 0f; 
-    //private float spawnPosZ = 10f; 
-    //private float startDelay = 0f; 
-    //private float spawnInterval = 1f;
+    private float minInterval = 2f;
+    private float maxInterval = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     { 
         SpawnRandomCar();
-        //InvokeRepeating("SpawnRandomCar", startDelay, spawnInterval); 
     } 
 
     // Update is called once per frame
     void Update() 
     { 
-        //if (Input.GetKeyDown(KeyCode.S)) { SpawnRandomCar(); } 
+
     }
          
     void SpawnRandomCar() 
     { 
         int randomIndex = Random.Range(0, carPrefabs.Length);
         Instantiate(carPrefabs[randomIndex],carSpawnPoint.position,carSpawnPoint.rotation,transform);
+        float nextInterval = Random.Range(minInterval, maxInterval);
+        Invoke(nameof(SpawnRandomCar), nextInterval);
         
     } 
 }
