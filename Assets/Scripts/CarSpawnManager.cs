fb6eb8fc1790bf46d9c48466119fb54701a1958e
hogstol03 <juliabhogstol@hotmail.com>
2026-05-09 21:42:56 +0200

    Cars spawn on the correct spawn point

--- Assets/Scripts/CarSpawnManager.cs
+++ Assets/Scripts/CarSpawnManager.cs
@@ -2,28 +2,30 @@ using UnityEngine;
 
 public class CarSpawnManager : MonoBehaviour
 {
-    public GameObject[] carPrefabs; 
-    private float spawnRangeX = 0f; 
-    private float spawnPosZ = 10f; 
-    private float startDelay = 0f; 
-    private float spawnInterval = 1f;
+    public GameObject[] carPrefabs;
+    public Transform carSpawnPoint;
+    //private float spawnRangeX = 0f; 
+    //private float spawnPosZ = 10f; 
+    //private float startDelay = 0f; 
+    //private float spawnInterval = 1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     { 
-        InvokeRepeating("SpawnRandomCar", startDelay, spawnInterval); 
+        SpawnRandomCar();
+        //InvokeRepeating("SpawnRandomCar", startDelay, spawnInterval); 
     } 
 
     // Update is called once per frame
     void Update() 
     { 
-        if (Input.GetKeyDown(KeyCode.S)) { SpawnRandomCar(); } 
+        //if (Input.GetKeyDown(KeyCode.S)) { SpawnRandomCar(); } 
     }
          
     void SpawnRandomCar() 
     { 
-        int carIndex = Random.Range(0, carPrefabs.Length); 
-        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); 
-        Instantiate(carPrefabs[carIndex], spawnpos, carPrefabs[carIndex].transform.rotation); 
+        int randomIndex = Random.Range(0, carPrefabs.Length);
+        Instantiate(carPrefabs[randomIndex],carSpawnPoint.position,carSpawnPoint.rotation,transform);
+        
     } 
 }
