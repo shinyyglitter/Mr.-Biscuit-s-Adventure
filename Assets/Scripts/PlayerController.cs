a3324906daa378a89fd8afa1ba480a5f853e2547
hogstol03 <juliabhogstol@hotmail.com>
2026-05-10 21:03:04 +0200

    Added a character boundary
    The cat now has boundaries on the sides

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -45,6 +45,12 @@ public class PlayerController : MonoBehaviour
         bool isWalking = horizontalInput != 0 || verticalInput != 0;
         animator.SetBool("Walking", isWalking);
         animator.SetBool("Idle", !isWalking);
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, -7f, 7f),
+            transform.position.y,
+            transform.position.z
+        );
     }
 
     private void OnCollisionEnter(Collision collision)
