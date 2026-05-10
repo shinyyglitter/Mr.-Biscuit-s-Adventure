3b492b07a2682425cca6e7261760f44f707462ec
Sunniva Knudsen <sunnknu0@gmail.com>
2026-05-10 20:10:23 +0200

    fixed PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -48,24 +48,33 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision collision)
-    bool isWalking = horizontalInput != 0 || verticalInput != 0;
-    animator.SetBool("Walking", isWalking);
-    animator.SetBool("Idle", !isWalking);
-    
-    if (Input.GetKeyDown(KeyCode.Space)&& isGrounded)
     {
-        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        isGrounded = false;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
-}
 
     void FixedUpdate()
     {
-    Vector3 move = transform.forward * verticalInput * speed;
-    Vector3 targetVelocity = transform.forward * verticalInput * speed;
+        if (!gameManager.isGameActive) return;
 
-    playerRb.linearVelocity = new Vector3(targetVelocity.x + playerRb.linearVelocity.x * 0f, playerRb.linearVelocity.y, targetVelocity.z + playerRb.linearVelocity.z * 0f);
+        Vector3 move = transform.forward * verticalInput * speed;
+        Vector3 targetVelocity = transform.forward * verticalInput * speed;
+    
+        playerRb.linearVelocity = new Vector3(targetVelocity.x + playerRb.linearVelocity.x * 0f, playerRb.linearVelocity.y, targetVelocity.z + playerRb.linearVelocity.z * 0f);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Point"))
+        {
+            pm.pointCount++;
+            Destroy(other.gameObject);
+            
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
