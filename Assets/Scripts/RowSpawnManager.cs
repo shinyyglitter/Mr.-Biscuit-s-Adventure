df1934159b2c16931c227f01c065e16e8f52d5e0
hogstol03 <juliabhogstol@hotmail.com>
2026-05-10 15:34:29 +0200

    Removed unnecessary code
    Just doing some tidying

--- Assets/Scripts/RowSpawnManager.cs
+++ Assets/Scripts/RowSpawnManager.cs
@@ -5,17 +5,8 @@ public class RowSpawnManager : MonoBehaviour
 { 
     public GameObject[] rowPrefabs; 
     public Transform lastRow;
-
     public int startRows =20;
-    public float rowSpacing =2f;
-
-    /*
-    private float spawnRangeX = 0f; 
-    private float spawnPosZ = 30f; 
-    private float startDelay = 0f; 
-    private float spawnInterval = 1f; 
-    */
-    
+    public float rowSpacing =2f; 
     public GameManager gameManager;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created 
@@ -28,13 +19,12 @@ public class RowSpawnManager : MonoBehaviour
     public void StartSpawning()
     {
         StartCoroutine(SpawnLoop());
-        //InvokeRepeating("SpawnRandomRow", startDelay, spawnInterval); 
     }
     
     // Update is called once per frame 
     void Update() 
     { 
-        //if (Input.GetKeyDown(KeyCode.S)) { SpawnRandomRow(); } 
+
     }
 
     IEnumerator SpawnLoop()
@@ -52,11 +42,5 @@ public class RowSpawnManager : MonoBehaviour
             : lastRow.position + Vector3.forward * rowSpacing;
 
         lastRow = Instantiate(rowPrefabs[Random.Range(0, rowPrefabs.Length)], pos, Quaternion.identity).transform;
-        
-            /*
-            int rowIndex = Random.Range(0, rowPrefabs.Length); 
-            Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); 
-            Instantiate(rowPrefabs[rowIndex], spawnpos, rowPrefabs[rowIndex].transform.rotation); 
-            */
     } 
 }
